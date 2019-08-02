b5deac28dc4c5949aa3216fefa09e83bff15ff8c
humphreyfonseca <humphrey.fonseca@gmail.com>
2019-08-02 16:34:01 -0300

    Schemas e Types

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,71 +1,27 @@
 import { makeExecutableSchema } from 'graphql-tools';
 
-const users: any[] = [
-    {
-        id: 1,
-        name: 'Jon',
-        email: 'jon@email.com'
-    },
-    {
-        id: 2,
-        name: 'Dany',
-        email: 'dany@email.com'
-    }
-];
+import { Query } from './query';
+import { Mutation } from './mutation';
 
+import { commentTypes } from './resources/comment/comment.schema';
+import { postTypes } from './resources/post/post.schema';
+import { userTypes } from './resources/user/user.schema';
 
-const typeDefs = `
-    type User{
-        id: ID!
-        name: String!
-        email: String!
-        photo: String
-        post:[Post!]!
-    }
-
-    type Post{
-        id: ID!
-        title: String!
-        content: String!
-        photo: String!
-        author: User!
-        comments: [Comment!]!
-    }
-
-    type Comment{
-        id: ID!
-        comment: String!
-        user: User!
-        post: Post!
-    }
-
-    type Query{
-        allUsers: [User!]!
-    }
-
-    type Mutation {
-        createUser(name: String!, email: String!): User
+const SchemaDefinition = `
+    type Schema {
+        query: Query
+        mutation: Mutation
     }
 `;
 
-const resolvers = {
-    //resolver trival exemplo
-    //nao necessario nesse caso
-    User:{
-        id:   (user) => user.id,
-        name: (user) => user.name,
-        email:(user) => user.email
-    },
-    Query:{
-        allUsers: () => users
-    },
-    Mutation:{
-        createUser: (parent, args) => {
-            const newUser = Object.assign({id: users.length+1}, args);
-            users.push(newUser);
-            return newUser;
-        }
-    }
-};
 
-export default makeExecutableSchema({typeDefs, resolvers});
+export default makeExecutableSchema({
+    typeDefs:[
+        SchemaDefinition,
+        Query,
+        Mutation,
+        commentTypes,
+        postTypes,
+        userTypes
+    ]
+});
