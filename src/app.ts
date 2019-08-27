aab6a457eb11d5483b2a2bf2da68034c6d6122b2
humphreyfonseca <humphrey.fonseca@gmail.com>
2019-08-27 17:58:37 -0300

    Adição CORS, HELMET, GZIP, CLUSTER

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import { DataLoaderFactory } from './graphql/dataloaders/DataLoaderFactory';
 import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
+import * as cors from 'cors';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
 
 import db from './models';
 import schema from './graphql/schema';
@@ -31,6 +34,17 @@ class App{
                 hello: 'Hello World!'
             })
         });*/
+
+        this.express.use(cors({
+            origin: '*',
+            methods: ['GET', 'POST'],
+            allowedHeaders: ['Content-Type', 'Authorization', 'Accept-Enconding'],
+            preflightContinue: false,
+            optionsSuccessStatus: 204
+        }));
+
+        this.express.use(compression());
+        this.express.use(helmet());
 
         this.express.use('/graphql', 
 
